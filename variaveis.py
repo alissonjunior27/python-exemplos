3848f78a90bc5dae7575ab72f34679530a66ccec
alisson_junior <alissonjrsantos7@gmail.com>
2024-04-23 16:39:06 -0300

    criação variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+nome= "Alisson Junior Gonçalves dos Santos"
+idade=16
+email= "alissonjrsantos7@gmail.com"
+telefone= '17 991582170'
+nota= 8,5
+aprovado= True
+print(nome)
+print(email)
+print(idade)
+print(telefone)
+print(nota)
+print(aprovado)
