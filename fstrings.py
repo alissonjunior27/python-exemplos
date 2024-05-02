93dd672201f2769b695951eb8ac93396f032cca2
alisson_junior <alissonjrsantos7@gmail.com>
2024-05-02 16:26:57 -0300

    fstrings2.py

--- fstrings.py
+++ fstrings.py
@@ -4,5 +4,5 @@ ano_atual = datetime.now().year
 clube = 'SCCP'
 ano_fundacao = 1910
 campeonato_mundial = 2
-print(f"{clube}possui{campeonato_mundial}titulos mundiais.")
-print(f"Corinthians {ano_atual - ano_fundação} anos de existendia.")
+print(f"{clube} possui {campeonato_mundial} titulos mundiais.")
+print(f"o clube tem {ano_atual - ano_fundacao} anos de existencia.")
