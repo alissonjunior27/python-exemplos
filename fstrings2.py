fb1e0e217dcf75f66c7620a943bff24b031859e1
alisson_junior <alissonjrsantos7@gmail.com>
2024-05-02 16:34:39 -0300

    Criação fstrings2.py

---
+++ fstrings2.py
@@ -0,0 +1,7 @@
+#Formação de Strings com várias linhas
+escola = 'Senai'
+curso = 'Desenvolvimento de Sistemas'
+uc = 'Lógica de Programação'
+print(f"Escola: {escola}\n"
+      f"Curso: {curso}\n"
+      f"Unidade Curricular: {uc}")
