f506e61ce48cd5d2844943e15109ac33eeb465ba
alisson_junior <alissonjrsantos7@gmail.com>
2024-05-21 14:58:22 -0300

    criacao listas.py

---
+++ listas.py
@@ -0,0 +1,8 @@
+aluno = ["alisson junior", 4, 8.5, True]
+print(f"nome: {aluno[0]}")
+print(f"faltas: {aluno[1]}")
+print(f"média: {aluno[2]}")
+if aluno[3] == True:
+    print(f"aprovado: sim")
+else:
+    print("aprovado: nao")
