d0f805ffc02f30b056b709009c7ab6242b6e54e1
alisson_junior <alissonjrsantos7@gmail.com>
2024-05-23 10:17:42 -0300

    criação clonar.py

---
+++ lista_clonar.py
@@ -0,0 +1,4 @@
+alunos=['joao', 'maria', 'pedro', 'ana', 'lucas', 'mariana']
+copia = list(alunos)
+print(f"lista orginal: {alunos}")
+print(f"lista copia:{copia}")
---
+++ listas_romover.py
@@ -0,0 +1,14 @@
+alunos=['adriana', 'joao', 'maria', 'pedro', 'ana', 'lucas', 'mariana', 'adriana']
+print(f"lista de alunos: {alunos}")
+# metodo remove()
+alunos.remove('adriana')
+print(f"lista de alunos: {alunos}")
+# metodo pop()
+alunos.pop(2)
+print(f"listas de alunos: {alunos}")
+# funçao del
+del alunos[3]
+print(f"lista de alunos: {alunos}")
+# metodo clear()
+alunos.clear()
+print(f"lista de alunos: {alunos}")
